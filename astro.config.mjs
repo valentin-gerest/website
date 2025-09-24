06ff5cd0a4a45941325ea5a3b22da7c7e1c51a63
Valentin GEREST <valentin@gerest.fr>
2025-09-24 12:28:14 +0200

    refactor: format project with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-   vite: {
-      plugins: [tailwindcss(), mdx(), sitemap()],
-   },
-   prefetch: {
-      prefetchAll: true
-   }
+  vite: {
+    plugins: [tailwindcss(), mdx(), sitemap()],
+  },
+  prefetch: {
+    prefetchAll: true,
+  },
 });
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,5 @@
 export const formatDate = (date: string | Date) => {
   return new Intl.DateTimeFormat("fr-FR", { dateStyle: "long" }).format(
-    new Date(date)
+    new Date(date),
   );
 };
